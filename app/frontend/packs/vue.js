99e3dc67c5d7559b46bacdf0ff70d45c304e27a7
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-09-28 12:39:09 +0200

    Remove almost all asset pipeline

--- app/frontend/packs/vue.js
+++ app/frontend/packs/vue.js
@@ -1,12 +1,10 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript/vue and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'vue' %> to the appropriate
-// layout file, like app/views/layouts/vue.html.erb
 console.log('Hello from application vue webpack');
+
+// Import coffee scss manifest
+import '../src/stylesheets/vue/vue';
+
+// Configure Stimulus
+import 'bootstrap/dist/js/bootstrap';
 
 import Rails from 'rails-ujs'
 import Turbolinks from 'turbolinks'
